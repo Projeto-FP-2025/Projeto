42fee8aeb65f2d73c8d1b05cce6ff218be3ea827
Luiza Costa <cwluiza029@gmail.com>
2025-11-10 21:17:06 -0300

    1 commit

---
+++ Organiza Festa.py
@@ -0,0 +1,46 @@
+import os
+
+print("[1] Adicionar uma nova festa")
+print("[2] Visualizar uma festa")
+print("[3] Editar uma festa")
+print("[4] Excluir uma festa")
+
+print("="*10)
+opc = int(input("digite uma opção: "))
+print("="*10)
+
+if opc == 1:
+    nome_de_arquivo = input("Digite o nome do arquivo(colocar .txt): ")
+    with open(nome_de_arquivo, 'w') as arquivo:
+        arquivo.write(input("Digite o tipo de festa: "))
+        arquivo.write(input("Digite a data da festa: "))
+        arquivo.write(input("Digite o local da festa: "))
+        arquivo.write(input("Digite o orçamento da festa: "))
+    print("Festa cadastrada com sucesso!")
+
+elif opc == 2:
+    nome_de_arquivo = input("Qual arquivo vc quer visualizar: ")
+    try:
+        with open(nome_de_arquivo) as arquivo:
+            print(arquivo.read())
+    except FileNotFoundError:
+        print("arquivo não encontrado.")
+
+elif opc == 3:
+    nome_de_arquivo = input("Qual arquivo vc quer editar: ")
+    try:
+        with open(nome_de_arquivo, 'w') as arquivo:
+            arquivo.write(input("Qual e o tipo da festa:"))
+            arquivo.write(input("Digite a data da festa"))
+            arquivo.write(input("Digite o local da festa"))
+            arquivo.write(input("Digite o orcamento da festa"))
+    except FileNotFoundError:
+        print("Arquivo nao existe")
+
+elif opc == 4:
+    nome_de_arquivo = input("Digite o nome do arquivo que deseja apagar: ")
+    os.remove(nome_de_arquivo)
+    print("Arquivo excluido com sucesso!")
+
+else:
+    print("Arquivo nao encontrado")
